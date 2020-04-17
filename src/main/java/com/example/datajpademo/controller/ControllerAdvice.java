6b00f4820b0c2cc2e8567c1d0f24859e366279df
polina.kotelnikova <polina.kotelnikova@compulink.ru>
2020-04-17 12:47:20 +0300

    Exception advice, logs

--- src/main/java/com/example/datajpademo/controller/ControllerAdvice.java
+++ src/main/java/com/example/datajpademo/controller/ControllerAdvice.java
@@ -1,7 +1,7 @@
 package com.example.datajpademo.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
@@ -11,24 +11,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class ControllerAdvice {
     private Logger logger = LoggerFactory.getLogger(ControllerAdvice.class);
 
     @Data
-    @AllArgsConstructor
-    public class ErrorInfo {
-        private LocalDateTime dateTime;
-        private String exception;
-        private String message;
+    @RequiredArgsConstructor
+    private class ErrorInfo {
+        private LocalDateTime timestamp = LocalDateTime.now();
+        private final String exception;
+        private final String error;
+        private final String message;
     }
 
-    @ResponseStatus(NOT_FOUND)
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(DataAccessException.class)
     public ErrorInfo handleDataAccessException(DataAccessException exception) {
         logger.error("Data Access Exception", exception);
-        return new ErrorInfo(LocalDateTime.now(), exception.getClass().getName(), "Ошибка при получении/сохранении данных.");
+        return new ErrorInfo(exception.getClass().getName(), "Ошибка при получении/сохранении данных.", exception.getLocalizedMessage());
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorInfo handleException(Exception ex) {
+        logger.error("Error!!!", ex);
+        return new ErrorInfo(ex.getClass().getName(), "Ошибка сервера.", ex.getLocalizedMessage());
     }
 }
