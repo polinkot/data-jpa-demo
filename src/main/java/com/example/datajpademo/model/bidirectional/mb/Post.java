f1127978f7c40b50458e10d491934b15b0664c56
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2020-08-10 16:13:17 +0300

    Refactoring

--- src/main/java/com/example/datajpademo/model/bidirectional/mb/Post.java
+++ src/main/java/com/example/datajpademo/model/bidirectional/mb/Post.java
@@ -5,12 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
