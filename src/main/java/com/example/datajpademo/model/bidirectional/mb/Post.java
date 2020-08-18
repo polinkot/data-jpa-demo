163d787eb72e839aa59f81075f362146aa42a3c9
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2020-08-18 17:45:34 +0300

    uniqueConstraints

--- src/main/java/com/example/datajpademo/model/bidirectional/mb/Post.java
+++ src/main/java/com/example/datajpademo/model/bidirectional/mb/Post.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -23,6 +20,9 @@ import static javax.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 @Builder
 @Entity
+
+@Table(name = "post", uniqueConstraints = @UniqueConstraint(name = "title_uq", columnNames = "title"))
+
 public class Post {
 
     @Id
